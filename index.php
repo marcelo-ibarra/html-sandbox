7cca1ad2d025f70875ea0a7fbd51b9c85fece5b9
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-07-12 12:50:24 -0600

    Anded index.php for html project

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<title>My Web Page</title>
+	</head>
+	<body>
+		<h1>Hai World!</h1>
+		<a href="https://go to google.com" target="_blank">Go to google.com</a>
+ 	</body>
+</html>
