09941bf2a3747c0a8d505ce6a4d34119885a04d9
Marcelo Ibarra <zulymar.mi@gmail.com>
2016-08-04 08:53:24 -0600

    A few changes before bootstrap lesson

--- index.php
+++ index.php
@@ -5,7 +5,33 @@
 		<title>My Web Page</title>
 	</head>
 	<body>
+		<header>
+			<h1>Hai World!</h1>
+		</header>
+		<link href="css/style.css" rel="stylesheet" type="text/scc"/>
+		<!-- adding custom javaScript
+	''<script>csr="js/custom.js</script>
+
+		<!-- Start Main>
+		<!-- Begin Header-->
 		<h1>Hai World!</h1>
-		<a href="https://go to google.com" target="_blank">Go to google.com</a>
+		<div class="foo">
+		sldfkasljwwiejf;ljflsdjfalsejfsdijlfkjlsdkjspdifjaefjsdijfwlekfjspdijsdjflsidjfsejslkdjfsidjflwkefjsodifsdkf<br>
+			sdijfsdijflsdkfjosaosdifjsodifjsoijfofasosdijfosd;asoidjfoasidoijasodifjsodifjsoidjfasijdfosidjfisdjfoisdjosidjfsidjfosd;<br>oifajlfijoij;oijoisjefowifjoidj;oaiejfwofj;adijfjkjdosdjfoeijfasdjfosijdfosijelfkjsldkfjsodiifjejfoijr;fiadnvncnfkelfifjwoirj<br>flodijvsdnfekfjoas;dkfwkfoaskdnflsdkjciw;elsldijfsod;falkeosijasdkjfkfeo;aosdjfkfs;dkjijasdklfjaslfeowefksldkmfosidj<br>fekwjlkmspoidjalkefmlkfmspodjfa;slkdmflekfms;dfkmselkfmedfiojas;dfjslekfmsdofjaslkfmsldkfsodijfa;seklfmsldkfjspodifja;wekm
+		</div>
+		<a href="https://google.com" target="_blank">Go to google.com</a>
+		<table>
+			<tr>
+				<td>
+				table cell 1
+				</td>
+				table cell 2
+				<td>
+				table cell 3
+					</td>table cell 4
+				</tr>
+			</table>
+				<footer>
+					&copy; 2016
  	</body>
 </html>
---
+++ js/custom.js
@@ -0,0 +1,3 @@
+/**
+ * Created by STEMulus on 7/12/2016.
+ */
